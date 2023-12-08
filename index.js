cc40161f13644f50b8710ebc8d4361e56b4b4f31
elainecarminhola <elainecgcarminhola@gamil.com>
2023-12-08 19:57:50 -0300

    created class cleint and class account

---
+++ index.js
@@ -0,0 +1,38 @@
+/** Created class the Client and current account */
+
+class Client{
+    name;
+    cpf;
+}
+
+class CurrentAccount{
+    agency;
+    numberAccount;
+    balance;
+}
+
+/** Instantiating classes and assigning values to objects*/
+
+const client1 = new Client();
+client1.name = 'Frederico Azevedo';
+client1.cpf = '1234565632';
+
+const client2 = new Client();
+client2.name = 'João de Lucca';
+client2.cpf = '1457898963';
+
+const account1 = new CurrentAccount();
+account1.agency = 123;
+account1.numberAccount = 1548762;
+account1.balance = 3500;
+
+const account2 = new CurrentAccount();
+account2.agency = 123;
+account2.numberAccount = 2584679;
+account2.balance = 9456;
+
+console.log(client1);
+console.log(client2);
+
+console.log(account1);
+console.log(account2);
