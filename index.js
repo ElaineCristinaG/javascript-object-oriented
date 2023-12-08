0a34a4ffbb01b3a1f074be0864baef9b3a87b683
elainecarminhola <elainecgcarminhola@gamil.com>
2023-12-08 20:24:53 -0300

    created methods operation bank

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/** Bank BytBank */
 /** Created class the Client and current account */
 
 class Client{
@@ -9,7 +10,25 @@ class CurrentAccount{
     agency;
     numberAccount;
     balance;
+
+    withdrawalOperation(value){
+        if(this.balance >= value){
+            this.balance -= value;
+            console.log('Successful withdrawal')
+        }else{
+            console.log('Insuficcient Balance ')
+        }
+    }
+
+    depositOperation(value){
+        if(value > 0){
+            this.balance += value;
+        console.log('Successful deposit')
+        } 
+    }
 }
+
+
 
 /** Instantiating classes and assigning values to objects*/
 
@@ -31,8 +50,13 @@ account2.agency = 123;
 account2.numberAccount = 2584679;
 account2.balance = 9456;
 
-console.log(client1);
-console.log(client2);
+/**call methods */
 
-console.log(account1);
+console.log("befor: ",account1);
+account1.withdrawalOperation(65);
+
+console.log(client1);
+console.log("after: ",account1);
+
+console.log(client2);
 console.log(account2);
