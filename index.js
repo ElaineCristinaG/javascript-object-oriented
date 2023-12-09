500d30eedbff57d1d8e06a4b7d94d75508f8f05f
elainecarminhola <elainecgcarminhola@gamil.com>
2023-12-09 09:27:04 -0300

    implementation pivates attributes

--- index.js
+++ index.js
@@ -9,21 +9,21 @@ class Client{
 class CurrentAccount{
     agency;
     numberAccount;
-    balance;
+    _balance;      // is one convention for representing private attributes 
 
     withdrawalOperation(value){
-        if(this.balance >= value){
-            this.balance -= value;
-            console.log('Successful withdrawal')
+        if(this._balance >= value){
+            this._balance -= value;
+            console.log('***Successful withdrawal***')
         }else{
-            console.log('Insuficcient Balance ')
+            console.log('***Insuficcient _balance***')
         }
     }
 
     depositOperation(value){
         if(value > 0){
-            this.balance += value;
-        console.log('Successful deposit')
+            this._balance += value;
+        console.log('***Successful deposit***')
         } 
     }
 }
@@ -34,7 +34,7 @@ class CurrentAccount{
 
 const client1 = new Client();
 client1.name = 'Frederico Azevedo';
-client1.cpf = '1234565632';
+client1.cpf = '1234565632'; 
 
 const client2 = new Client();
 client2.name = 'João de Lucca';
@@ -43,20 +43,27 @@ client2.cpf = '1457898963';
 const account1 = new CurrentAccount();
 account1.agency = 123;
 account1.numberAccount = 1548762;
-account1.balance = 3500;
+account1._balance = 0;
 
 const account2 = new CurrentAccount();
 account2.agency = 123;
 account2.numberAccount = 2584679;
-account2.balance = 9456;
+account2._balance = 0;
 
 /**call methods */
 
 console.log("befor: ",account1);
+account1.depositOperation(200);
+console.log("after: ",account1);
 account1.withdrawalOperation(65);
+console.log("after: ",account1);
 
 console.log(client1);
 console.log("after: ",account1);
 
 console.log(client2);
-console.log(account2);
+console.log(account2); 
+
+console.log(account2)
+account2.depositOperation(2500);
+console.log(account2)
