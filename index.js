bfbdd717f3fd28fe766fd9dc1a71b70d0d93c59d
elainecarminhola <elainecgcarminhola@gamil.com>
2023-12-09 09:59:45 -0300

    implementation return methods

--- index.js
+++ index.js
@@ -15,20 +15,21 @@ class CurrentAccount{
         if(this._balance >= value){
             this._balance -= value;
             console.log('***Successful withdrawal***')
+            return value;
         }else{
-            console.log('***Insuficcient _balance***')
+            console.log('***Insuficcient balance***')
+            return this._balance;
         }
     }
 
     depositOperation(value){
         if(value > 0){
             this._balance += value;
-        console.log('***Successful deposit***')
+            console.log('***Successful deposit***')
+            return value;
         } 
     }
 }
-
-
 
 /** Instantiating classes and assigning values to objects*/
 
@@ -36,34 +37,16 @@ const client1 = new Client();
 client1.name = 'Frederico Azevedo';
 client1.cpf = '1234565632'; 
 
-const client2 = new Client();
-client2.name = 'João de Lucca';
-client2.cpf = '1457898963';
-
 const account1 = new CurrentAccount();
 account1.agency = 123;
 account1.numberAccount = 1548762;
 account1._balance = 0;
 
-const account2 = new CurrentAccount();
-account2.agency = 123;
-account2.numberAccount = 2584679;
-account2._balance = 0;
-
 /**call methods */
+const valueDeposit = account1.depositOperation(2500);
+console.log("Deposito: ",valueDeposit)
+console.log(account1)
 
-console.log("befor: ",account1);
-account1.depositOperation(200);
-console.log("after: ",account1);
-account1.withdrawalOperation(65);
-console.log("after: ",account1);
-
-console.log(client1);
-console.log("after: ",account1);
-
-console.log(client2);
-console.log(account2); 
-
-console.log(account2)
-account2.depositOperation(2500);
-console.log(account2)
+const valueWithdrawal = account1.withdrawalOperation(1000);
+console.log("Saque: ",valueWithdrawal)
+console.log(account1)
